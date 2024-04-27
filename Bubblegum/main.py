dec1cb7c176cc49191015c3e1d5c3e984112929c
tomrosemm <tomrose@umich.edu>
2024-04-27 18:01:02 -0400

    1

--- Bubblegum/main.py
+++ Bubblegum/main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 # Supported modes : Light, Dark, System
 ctk.set_appearance_mode("System")
 
+
 # Supported themes : green, dark-blue, blue
 ctk.set_default_color_theme("green")
 
