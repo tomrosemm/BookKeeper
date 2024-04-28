f2da561d271ce16a9a2905f791de6b189e841494
tomrosemm <tomrose@umich.edu>
2024-04-28 00:29:41 -0400

    split it out

---
+++ admintools.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import subprocess
+
+
+def run_script1():
+    script_path = 'frontEndStartUp.py'
+    subprocess.run(['python', script_path])
+
+
+def run_script2():
+    script_path = 'tools.py'
+    subprocess.run(['python', script_path])
+
+
+def button1_clicked():
+    run_script1()
+
+
+def button2_clicked():
+    run_script2()
+
+
+# Create the main window
+root = tk.Tk()
+root.title("Admin Tools")
+
+# Create the buttons
+button1 = tk.Button(root, text="Initial Setup", width=30, height=15, command=button1_clicked)
+button2 = tk.Button(root, text="Tools", width=30, height=15, command=button2_clicked)
+
+# Create the labels
+label1 = tk.Label(root, text="")
+label2 = tk.Label(root, text="")
+
+# Grid layout
+button1.grid(row=0, column=0, padx=75, pady=75)
+button2.grid(row=0, column=1, padx=75, pady=75)
+label1.grid(row=1, column=0)
+label2.grid(row=1, column=1)
+
+# Start the main event loop
+root.mainloop()
